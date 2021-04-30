8080bd851ef15fe04f67931c28657709f1fd53e4
PauloAlves8039 <pj38alves@gmail.com>
2021-04-29 21:25:10 -0300

    Ajustando elementos do teclado

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -10,14 +10,32 @@ class Keyboard extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ButtonRow([
+            Button.big(text: "AC"),
+            Button(text: "%"),
+            Button.opration(text: "/"),
+          ]),
+          ButtonRow([
             Button(text: "7"),
             Button(text: "8"),
             Button(text: "9"),
+            Button.opration(text: "x"),
           ]),
           ButtonRow([
             Button(text: "4"),
             Button(text: "5"),
             Button(text: "6"),
+            Button.opration(text: "-"),
+          ]),
+          ButtonRow([
+            Button(text: "1"),
+            Button(text: "2"),
+            Button(text: "3"),
+            Button.opration(text: "+"),
+          ]),
+          ButtonRow([
+            Button.big(text: "0"),
+            Button(text: "."),
+            Button.opration(text: "="),
           ]),
         ],
       ),
